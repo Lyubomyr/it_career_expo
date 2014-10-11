a0f99641fb88a27c0ceea4c08ae3a190b0aa7c00
Lyubomyr <fedorniakl@gmail.com>
2014-10-11 15:34:00 +0300

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ItCareerExpo::Application.config.secret_key_base = '94204f37bd56e0cfed3608c63349f4ee2408176612f8a62ad770151f7bb4848eabba46585e1af1548ef5d4ffd56585258e2976f615d80988347d4a90a07fbbf9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ItCareerExpo::Application.config.session_store :cookie_store, key: '_it_career_expo_session'
